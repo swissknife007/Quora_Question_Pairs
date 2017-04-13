beefa6f4f4025d2f0d6c9f0d347984e775e2464c
Abhishek Jindal <ajindal121@gmail.com>
2017-04-13 05:49:47 +0000

    Adding data augmentation

---
+++ src/data_augmentation.py
@@ -0,0 +1,58 @@
+from random import random
+from random import randint
+import numpy as np
+
+def augment_data(x, y, z, xlen, ylen):
+    x_ad = []
+    y_ad = []
+    z_ad = []
+    xlen_ad = []
+    ylen_ad = []
+    for i in xrange(len(x)):
+    # print type(z[i])
+    # print z[i].shape
+    # print z[i]
+        if np.argmax(z[i]) == 1:
+            outcome = random() < 0.5
+            if outcome:
+                # augment
+                id = randint(1, 3)
+                if id == 1:
+                    # use question 1
+                    x_ad.append(x[i])
+                    y_ad.append(x[i])
+                    z_ad.append(z[i])
+                    xlen_ad.append(xlen[i])
+                    ylen_ad.append(ylen[i])
+                elif id == 2:
+                    # use question 2
+                    x_ad.append(y[i])
+                    y_ad.append(y[i])
+                    z_ad.append(z[i])
+                    xlen_ad.append(xlen[i])
+                    ylen_ad.append(ylen[i])
+                else:
+                    # swap questions
+                    x_ad.append(y[i])
+                    y_ad.append(x[i])
+                    z_ad.append(z[i])
+                    xlen_ad.append(xlen[i])
+                    ylen_ad.append(ylen[i])
+            else:
+                x_ad.append(x[i])
+                y_ad.append(y[i])
+                z_ad.append(z[i])
+                xlen_ad.append(xlen[i])
+                ylen_ad.append(ylen[i])
+        else:
+            x_ad.append(x[i])
+            y_ad.append(y[i])
+            z_ad.append(z[i])
+            xlen_ad.append(xlen[i])
+            ylen_ad.append(ylen[i])
+
+    assert(len(x) == len(x_ad)), 'len(x) != len(x_ad)'
+    assert(len(y) == len(y_ad)), 'len(y) != len(y_ad)'
+    assert(len(z) == len(z_ad)), 'len(z) != len(z_ad)'
+
+    return x_ad, y_ad, z_ad, xlen_ad, ylen_ad
---
+++ src/rnn_attention_two_way_attention_kaggle_augmented.py
@@ -0,0 +1,442 @@
+
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow.contrib import rnn
+# from tensorflow.contrib.keras import utils
+from keras.utils import np_utils
+import numpy as np
+from data_helpers_kaggle import fitData, fit_test_data, write_submission_file
+import argparse
+import sys
+import time
+import random
+from cnn_kaggle import get_cnn_embedding
+from sklearn.preprocessing.data import OneHotEncoder
+from data_augmentation import augment_data
+
+
+max_len = 40
+
+# Parameters
+learning_rate = 0.001
+training_iters = 100000
+batch_size = 512
+display_step = 10
+
+# Network Parameters
+n_input = 300
+n_steps = max_len  # timesteps
+n_hidden = 100  # hidden layer num of features
+n_classes = 2
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_steps, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+# Define weights
+weights = {
+    # Hidden layer weights => 2*n_hidden because of forward + backward cells
+    'out': tf.Variable(tf.random_normal([2 * n_hidden, n_classes]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+def get_params():
+    parser = argparse.ArgumentParser(description = 'Short sample app')
+    parser.add_argument('-lstm', action = "store", default = 300, dest = "lstm_units", type = int)
+    parser.add_argument('-epochs', action = "store", default = 35, dest = "epochs", type = int)
+    parser.add_argument('-batch', action = "store", default = 512, dest = "batch_size", type = int)
+    parser.add_argument('-emb', action = "store", default = 300, dest = "emb", type = int)
+    parser.add_argument('-maxlen', action = "store", default = 40, dest = "maxlen", type = int)
+    parser.add_argument('-maxfeat', action = "store", default = 2000000, dest = "max_features", type = int)
+    parser.add_argument('-classes', action = "store", default = 2, dest = "num_classes", type = int)
+    parser.add_argument('-lr', action = "store", default = 0.001, dest = "lr", type = float)
+    parser.add_argument('-verbose', action = "store", default = False, dest = "verbose", type = bool)
+#     parser.add_argument('-train', action="store", default="train_all.txt", dest="train")
+#     parser.add_argument('-test', action="store", default="test_all.txt", dest="test")
+#     parser.add_argument('-dev', action="store", default="dev.txt", dest="dev")
+
+    opts = parser.parse_args(sys.argv[1:])
+    print ("lstm_units", opts.lstm_units)
+    print ("epochs", opts.epochs)
+    print ("batch_size", opts.batch_size)
+    print ("emb", opts.emb)
+    print ("maxlen", opts.maxlen)
+    print ("max_features", opts.max_features)
+    return opts
+
+class AttentionModel:
+
+    def __init__(self, opts, sess, MAXLEN, vocab, embedding_matrix, num_filters = 128, filter_sizes = [3, 4, 5], batch_size = 512):
+        self.dim = 300
+        self.sess = sess
+        self.h_dim = opts.lstm_units
+        self.batch_size = batch_size
+        self.vocab_size = len(vocab)
+        self.init_emb_matrix = embedding_matrix
+        self.MAXLEN = MAXLEN
+        self.filter_sizes = filter_sizes
+        self.num_filters = num_filters
+
+    def build_model(self):
+
+        self.x = tf.placeholder(tf.int32, [self.batch_size, self.MAXLEN], name = "premise")
+
+        self.x_length = tf.placeholder(tf.int32, [self.batch_size], name = "premise_len")
+
+        self.y = tf.placeholder(tf.int32, [self.batch_size, self.MAXLEN], name = "hypothesis")
+
+        self.y_length = tf.placeholder(tf.int32, [self.batch_size], name = "hyp_len")
+
+        self.target = tf.placeholder(tf.float32, [self.batch_size, 2], name = "label")
+
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name = "dropout_keep_prob")
+
+        self.W = tf.Variable(tf.constant(0.0, shape = [self.vocab_size, self.dim]),
+                        trainable = True, name = "W")
+
+        # self.embedding_placeholder = tf.placeholder(tf.float32, [self.vocab_size, self.dim], name = 'emb_matrix')
+
+        self.embed_matrix = self.W.assign(self.init_emb_matrix)
+
+        self.x_emb = tf.nn.embedding_lookup(self.embed_matrix, self.x)
+
+        self.y_emb = tf.nn.embedding_lookup(self.embed_matrix, self.y)
+
+        self.W_Y = tf.get_variable("W_Y", shape = [self.h_dim, self.h_dim])
+
+        self.W_h = tf.get_variable("W_h", shape = [self.h_dim, self.h_dim])
+
+        self.W_r = tf.get_variable("W_r", shape = [self.h_dim, self.h_dim])
+
+        self.W_x = tf.get_variable("W_x", shape = [self.h_dim, self.h_dim])
+
+        self.W_p = tf.get_variable("W_p", shape = [self.h_dim, self.h_dim])
+
+        self.W_t = tf.get_variable("W_t", shape = [self.h_dim, self.h_dim])
+
+        self.W_att = tf.get_variable("W_att", shape = [self.h_dim, 1])
+
+        self.r_0 = tf.get_variable("r", initializer = tf.ones([self.batch_size, self.h_dim], dtype = tf.float32))
+
+        with tf.variable_scope("encode_q1"):
+            self.fwd_lstm = rnn.BasicLSTMCell(self.h_dim, state_is_tuple = True)
+
+            self.x_output, self.x_state = tf.nn.dynamic_rnn(cell = self.fwd_lstm, inputs = self.x_emb, dtype = tf.float32)
+
+        with tf.variable_scope("encode_q2"):
+
+            self.fwd_lstm = rnn.BasicLSTMCell(self.h_dim, state_is_tuple = True)
+
+            self.y_output, self.y_state = tf.nn.dynamic_rnn(cell = self.fwd_lstm, inputs = self.y_emb,
+                                                            initial_state = self.x_state, dtype = tf.float32)
+
+
+        self.Y = self.x_output  # its length must be x_length
+
+        self.H = self.y_output  # the set of hidden states from Q2
+
+        self.h_star = []
+
+        for i in xrange(2):
+
+            self.r = self.r_0
+
+            self.H_transposed = tf.transpose(self.H, perm = [1, 0, 2])
+
+            tmp5 = tf.transpose(self.y_output, [1, 0, 2])
+
+            self.h_n = tf.gather(tmp5, int(tmp5.get_shape()[0]) - 1)
+
+            tmp1 = tf.matmul(tf.reshape(self.Y, shape = [self.batch_size * self.MAXLEN, self.h_dim]), self.W_Y, name = "Wy")
+            self.Wy = tf.reshape(tmp1, shape = [self.batch_size, self.MAXLEN, self.h_dim])
+
+            for t in xrange(1, self.MAXLEN):
+
+                h_t = self.H_transposed[t:t + 1, :, ]
+
+                h_t_reshaped = tf.reshape(h_t, shape = [self.batch_size, self.h_dim])
+
+                h_t_repeat = tf.expand_dims(h_t_reshaped, 1)
+
+                pattern = tf.stack([1, self.MAXLEN, 1])
+
+                h_t_repeat = tf.tile(h_t_repeat, pattern)
+
+                tmp2 = tf.matmul(tf.reshape(h_t_repeat, shape = [self.batch_size * self.MAXLEN, self.h_dim]), self.W_h)
+
+                Wh_t = tf.reshape(tmp2, shape = [self.batch_size, self.MAXLEN, self.h_dim], name = "Wh_t")
+
+                r_repeat = tf.expand_dims(self.r, 1)
+
+                r_repeat = tf.tile(r_repeat, pattern)
+
+                tmp3 = tf.matmul(tf.reshape(r_repeat, shape = [self.batch_size * self.MAXLEN, self.h_dim]), self.W_r)
+
+                Wr_r = tf.reshape(tmp3, shape = [self.batch_size, self.MAXLEN, self.h_dim], name = "Wr_r")
+
+                M_input = tf.add(self.Wy, tf.add(Wh_t, Wr_r))
+
+                M_t = tf.tanh(M_input)
+
+                tmp4 = tf.matmul(tf.reshape(M_t, shape = [self.batch_size * self.MAXLEN, self.h_dim]), self.W_att)
+
+                att = tf.nn.softmax(tf.reshape(tmp4, shape = [self.batch_size, 1, self.MAXLEN], name = "att"))
+
+                self.att = att
+
+                r_future = tf.reshape(tf.matmul(att, self.Y), shape = [self.batch_size, self.h_dim])
+
+                Wt_r = tf.matmul(self.r, self.W_t, name = "Wt_r")
+
+                self.r = tf.add(r_future, tf.tanh(Wt_r))
+
+            self.Wpr = tf.matmul(self.r, self.W_p, name = "Wpr")
+
+            self.Wxhn = tf.matmul(self.h_n, self.W_x, name = "Wxhn")
+
+            self.h_star.append(tf.tanh(tf.add(self.Wpr, self.Wxhn), name = "hstar"))
+
+            self.Y = self.y_output  # its length must be x_length
+
+            self.H = self.x_output  # the set of hidden states from Q2
+
+        self.hstar_two_way = tf.concat(self.h_star, axis = 1)
+
+        self.h_star_x_cnn = get_cnn_embedding(self.x_emb, self.dropout_keep_prob, self.MAXLEN, self.dim, self.filter_sizes, self.num_filters)
+
+        print('h_star_x_cnn: ', self.h_star_x_cnn.shape)
+
+        self.h_star_y_cnn = get_cnn_embedding(self.y_emb, self.dropout_keep_prob, self.MAXLEN, self.dim, self.filter_sizes, self.num_filters)
+
+        print('h_star_y_cnn: ', self.h_star_y_cnn.shape)
+
+        self.W_pred = tf.get_variable("W_pred", shape = [ 2 * self.h_dim, 2 ])
+
+        self.scaled_pred = tf.nn.softmax(tf.matmul(self.hstar_two_way, self.W_pred), name = "pred_layer")
+
+        self.unscaled_pred = tf.matmul(self.hstar_two_way, self.W_pred)
+        # print "pred",self.pred,"target",self.target
+
+        class_weights = tf.div(tf.reduce_sum(self.target, 0), tf.constant(float(self.batch_size)))
+
+        weighted_logits = tf.multiply(self.unscaled_pred, class_weights)  # shape [batch_size, 2]
+
+        self.loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits = weighted_logits, labels = self.target, name = "loss"))
+
+        self.predictions = tf.argmax(self.scaled_pred, 1)
+
+        self.predictions_probs = self.scaled_pred[:, 1]
+
+        correct = tf.equal(self.predictions, tf.argmax(self.target, 1))
+
+        self.acc = tf.reduce_mean(tf.cast(correct, "float"), name = "accuracy")
+
+        self.optimizer = tf.train.AdamOptimizer()
+
+        self.optim = self.optimizer.minimize(self.loss, var_list = tf.trainable_variables())
+
+        _ = tf.summary.scalar("loss", self.loss)
+
+    def train(self, \
+              xdata, ydata, zdata, x_lengths, y_lengths, \
+              xdevdata, ydevdata, zdevdata, xdev_lengths, ydev_lengths, \
+              xxdata, yydata, zzdata, xx_lengths, yy_lengths, \
+              MAXITER):
+
+        merged_sum = tf.summary.merge_all()
+
+        # writer = tf.train.SummaryWriter("./logs/%s" % "modeldir", self.sess.graph_def)
+
+        tf.initialize_all_variables().run()
+
+        start_time = time.time()
+
+        best_val_loss = 1e100
+
+        for ITER in range(MAXITER):
+            print('**************EPOCH****************\n', str(ITER))
+            epoch_start_time = time.time()
+            total_loss = 0
+            # xdata, ydata, zdata, x_lengths, y_lengths = joint_shuffle(xdata, ydata, zdata, x_lengths, y_lengths)
+            for i in xrange(0, len(xdata), self.batch_size):
+                x, y, z, xlen, ylen = xdata[i:i + self.batch_size], \
+                                ydata[i:i + self.batch_size], \
+                                zdata[i:i + self.batch_size], \
+                                x_lengths[i:i + self.batch_size], \
+                                y_lengths[i:i + self.batch_size]
+
+                x, y, z, xlen, ylen = augment_data(x, y, z, xlen, ylen)
+
+                feed_dict = {self.x: x, \
+                             self.y: y, \
+                             self.target: z, \
+                             self.x_length:xlen, \
+                             self.y_length:ylen }
+
+                att, _ , loss, acc, summ = self.sess.run([self.att, self.optim, self.loss, self.acc, merged_sum], feed_dict = feed_dict)
+
+                total_loss += loss
+
+            print("Epoch Time: ", time.time() - epoch_start_time)
+            
+            total_loss = total_loss / float(len(xdata))
+            print ("Loss", total_loss, "Accuracy On Training", acc)
+            
+            total_val_loss = self.validate(xdevdata, ydevdata, zdevdata, xdev_lengths, ydev_lengths)
+
+            if(best_val_loss > total_val_loss):
+		best_val_loss = total_val_loss 
+            	self.test(xxdata, yydata, zzdata, xx_lengths, yy_lengths, ITER)
+
+        elapsed_time = time.time() - start_time
+
+        print("Total Time", elapsed_time)
+
+    def test(self, \
+              xxdata, yydata, zzdata, xx_lengths, yy_lengths, epoch_number):
+
+        merged_sum = tf.summary.merge_all()
+
+        start_time = time.time()
+
+        print('**********TESTING STARTED**********')
+        test_predictions = [0]*len(xxdata)
+
+        for i in xrange(0, len(xxdata), self.batch_size):
+            x, y, z, xlen, ylen = xxdata[i:i + self.batch_size], \
+                            yydata[i:i + self.batch_size], \
+                            zzdata[i:i + self.batch_size], \
+                            xx_lengths[i:i + self.batch_size], \
+                            yy_lengths[i:i + self.batch_size]
+
+            tfeed_dict = {self.x: x, \
+                          self.y: y, \
+                          self.target: z, \
+                          self.x_length:xlen, \
+                          self.y_length:ylen}
+
+            test_loss, att, test_acc, summ, test_predictions[i : i+self.batch_size] = self.sess.run([self.loss, self.att, self.acc, merged_sum, self.predictions_probs], feed_dict = tfeed_dict)
+
+            # print ('Test batches processed: ', (i / batch_size))
+            
+            #test_predictions.extend(test_preds)
+        print('............Test loss.....', test_loss/float(len(xxdata)))
+        print('**********TESTING ENDED**********')
+
+        write_submission_file(test_predictions, '../data/submissions', epoch_number)
+
+        elapsed_time = time.time() - start_time
+
+        print("Total Time", elapsed_time)
+
+    def validate(self, \
+              xxdata, yydata, zzdata, xx_lengths, yy_lengths):
+
+        merged_sum = tf.summary.merge_all()
+
+        start_time = time.time()
+
+        print('**********VALIDATION STARTED**********')
+        test_predictions = [0]*len(xxdata)
+
+        for i in xrange(0, len(xxdata), self.batch_size):
+            x, y, z, xlen, ylen = xxdata[i:i + self.batch_size], \
+                            yydata[i:i + self.batch_size], \
+                            zzdata[i:i + self.batch_size], \
+                            xx_lengths[i:i + self.batch_size], \
+                            yy_lengths[i:i + self.batch_size]
+
+            tfeed_dict = {self.x: x, \
+                          self.y: y, \
+                          self.target: z, \
+                          self.x_length:xlen, \
+                          self.y_length:ylen}
+
+            test_loss, att, test_acc, summ, test_predictions[i : i+self.batch_size] = self.sess.run([self.loss, self.att, self.acc, merged_sum, self.predictions_probs], feed_dict = tfeed_dict)
+
+            # print ('Test batches processed: ', (i / batch_size))
+            
+            #test_predictions.extend(test_preds)
+        print('...........Validation loss.....', test_loss/float(len(xxdata)))
+        print('**********VALIDATION ENDED**********')
+
+        
+
+        elapsed_time = time.time() - start_time
+
+        print("Total Time", elapsed_time)
+
+        return test_loss
+
+def joint_shuffle(xdata, ydata, zdata, x_lengths, y_lengths):
+
+    tmp = list(zip(xdata, ydata, zdata, x_lengths, y_lengths))
+
+    random.shuffle(tmp)
+
+    xdata, ydata, zdata, x_lengths, y_lengths = zip(*tmp)
+
+    return xdata, ydata, zdata, x_lengths, y_lengths
+
+if __name__ == "__main__":
+
+    options = get_params()
+
+    X_train, Y_train, Z_train, X_dev, Y_dev, Z_dev, vocab_dict, glove_matrix = fitData()
+
+    test_ids, X_test, Y_test, Z_test = fit_test_data()
+
+    X_train_lengths = [len(x) for x in X_train]
+
+    X_train_lengths = np.asarray([len(x) for x in X_train]).reshape(len(X_train))
+
+    X_dev_lengths = [len(x) for x in X_dev]
+
+    X_dev_lengths = np.asarray([len(x) for x in X_dev]).reshape(len(X_dev))
+    
+    X_test_lengths = [len(x) for x in X_test]
+
+    X_test_lengths = np.asarray([len(x) for x in X_test]).reshape(len(X_test)) 
+
+
+    
+    Y_train_lengths = [len(x) for x in Y_train]
+
+    Y_train_lengths = np.asarray([len(x) for x in Y_train]).reshape(len(Y_train))
+
+    Y_dev_lengths = [len(x) for x in Y_dev]
+
+    Y_dev_lengths = np.asarray([len(x) for x in Y_dev]).reshape(len(Y_dev))
+    
+    Y_test_lengths = [len(x) for x in Y_test]
+
+    Y_test_lengths = np.asarray([len(x) for x in Y_test]).reshape(len(Y_test)) 
+
+
+    
+    Z_train = np_utils.to_categorical(Z_train, nb_classes = options.num_classes)
+
+    Z_test = np_utils.to_categorical(Z_test, nb_classes = options.num_classes)
+
+    Z_dev = np_utils.to_categorical(Z_dev, nb_classes = options.num_classes)
+
+    MAXLEN = options.maxlen
+
+    MAXITER = options.epochs
+
+    with tf.Session() as sess:
+
+        model = AttentionModel(options, sess, MAXLEN, vocab_dict, glove_matrix, batch_size = 512)
+
+        model.build_model()
+
+        model.train(X_train, Y_train, Z_train, X_train_lengths, Y_train_lengths, \
+                    X_dev, Y_dev, Z_dev, X_dev_lengths, Y_dev_lengths, \
+                    X_test, Y_test, Z_test, X_test_lengths, Y_test_lengths, \
+                    MAXITER)
+
+#         model.test(X_test, Y_test, Z_test, X_test_lengths, Y_test_lengths, \
+#               glove_matrix)
